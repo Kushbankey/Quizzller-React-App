1e6e6fab0995f8260f00c91c0157b60acae9ea83
Kushagra Bankey <kushagrabankey1703@gmail.com>
2024-01-12 14:36:55 +0530

    minor changes

--- src/components/Questions.js
+++ src/components/Questions.js
@@ -1,9 +1,10 @@
 import Options from "./Options";
 
 function Questions({ filterQuestions, dispatch, answer }) {
+  console.log(filterQuestions)
   return (
     <div>
-      <h4>{filterQuestions.filterQuestions}</h4>
+      <h4>{filterQuestions.question}</h4>
 
       <Options filterQuestions={filterQuestions} dispatch={dispatch} answer={answer} />
     </div>
